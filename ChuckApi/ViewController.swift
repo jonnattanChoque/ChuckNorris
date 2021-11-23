7e890805752aa6c7daaa7967f7a17a2fb16f0b0b
JonnattanChoque <jdevelopertwon@gmail.com>
2021-11-23 18:13:42 -0500

    Initial Commit

---
+++ ChuckApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChuckApi
+//
+//  Created by jonnattan Choque on 23/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ChuckApiUITests/ChuckApiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ChuckApiUITestsLaunchTests.swift
+//  ChuckApiUITests
+//
+//  Created by jonnattan Choque on 23/11/21.
+//
+
+import XCTest
+
+class ChuckApiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
